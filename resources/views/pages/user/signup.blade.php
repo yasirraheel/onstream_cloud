79391e9a031de73a623ce2d682fcb32b23278943
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-01-13 19:22:31 +0500

    Hide WhatsApp consent checkbox on signup when OTP setting is disabled

--- resources/views/pages/user/signup.blade.php
+++ resources/views/pages/user/signup.blade.php
@@ -89,12 +89,14 @@ function verifyCaptcha() {
               <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="{{trans('words.confirm_password')}}">
             </div>
 
+            @if(getcong('wa_otp_verification') == 1)
             <div class="form-group">
                 <div class="custom-control custom-checkbox">
                     <input type="checkbox" class="custom-control-input" id="whatsapp_consent" name="whatsapp_consent" value="1">
                     <label class="custom-control-label" for="whatsapp_consent">Receive WhatsApp notifications</label>
                 </div>
             </div>
+            @endif
 
             @if(getcong('recaptcha_on_signup'))
             <div class="form-group">
